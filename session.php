2333a5252d8068c6c011292676075bf8f3757c27
kolaqsq <kolaqsq1707@gmail.com>
2020-06-25 15:37:35 +0300

    Started redaction feature

--- session.php
+++ session.php
@@ -1,3 +1,17 @@
+<?php
+function addData($id, $data, $desc)
+{
+
+
+
+    mysqli_query($data, "insert into questions(session_id, type, description) values ($id, 1, $desc)");
+//    header("Location: admin.php", true, 301);
+//    exit();
+}
+
+$database = mysqli_connect('std-mysql', 'std_950', '901109qsq', 'std_950');
+
+?>
 <!doctype html>
 <html lang="ru">
 <head>
@@ -8,6 +22,14 @@
     <title>Цифровые сессии. Редактирование</title>
 </head>
 <body>
+<?php
 
+?>
+<form action="<?php addData($_GET['session_id'], $database, $_POST['1']) ?>" method="post">
+    <label for="1">Добавьте вопрос №1</label>
+    <input type="number" name="1" id="1">
+
+    <br><input type="submit" value="Сохранить">
+</form>
 </body>
 </html>
