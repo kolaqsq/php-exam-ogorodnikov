d9b5a66a6bf8844863d3c145a4549499eeb7f389
kolaqsq <kolaqsq1707@gmail.com>
2020-06-25 15:58:34 +0300

    Another fix

--- session.php
+++ session.php
@@ -3,7 +3,6 @@ function addData($id, $data, $desc)
 {
 
 
-
     mysqli_query($data, "insert into questions(session_id, type, description) values ($id, 1, $desc)");
 //    header("Location: admin.php", true, 301);
 //    exit();
@@ -23,11 +22,28 @@ $database = mysqli_connect('std-mysql', 'std_950', '901109qsq', 'std_950');
 </head>
 <body>
 <?php
+if (!isset($_POST['first']))
+    $_POST['first'] = '';
+if (!isset($_POST['second']))
+    $_POST['second'] = '';
+if (!isset($_POST['third']))
+    $_POST['third'] = '';
+if (!isset($_POST['forth']))
+    $_POST['forth'] = '';
 
 ?>
-<form action="<?php addData($_GET['session_id'], $database, $_POST['1']) ?>" method="post">
-    <label for="1">Добавьте вопрос №1</label>
-    <input type="number" name="1" id="1">
+<form action="<?php addData($_GET['session_id'], $database, $_POST['first']) ?>" method="post">
+    <label for="first">Добавьте вопрос №1</label>
+    <input type="number" name="first" id="first"><br>
+
+    <label for="second">Добавьте вопрос №2</label>
+    <input type="number" min="0" name="second" id="second"><br>
+
+    <label for="third">Добавьте вопрос №3</label>
+    <input type="text" maxlength="30" name="third" id="third"><br>
+
+    <label for="forth">Добавьте вопрос №4</label>
+    <textarea name="forth" id="forth" cols="30" rows="10" maxlength="255"></textarea>
 
     <br><input type="submit" value="Сохранить">
 </form>
